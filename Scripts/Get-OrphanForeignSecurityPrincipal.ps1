dbab82c4b96f816223bacce7cb1072426c58a691
Luc FULLENWARTH <luke.forums@outlook.com>
2017-10-17 22:18:28 +0200

    Update Get-OrphanForeignSecurityPrincipal.ps1
    Removed Set-StrictMode
    Fixed Debug message
    A warning message is displayed when the Get-ADTrust cmdlet is not present.

--- Scripts/Get-OrphanForeignSecurityPrincipal.ps1
+++ Scripts/Get-OrphanForeignSecurityPrincipal.ps1
@@ -1,4 +1,4 @@
-﻿Function Get-OrphanForeignSecurityPrincipal
+Function Get-OrphanForeignSecurityPrincipal
 {
     <#
     .SYNOPSIS
@@ -16,11 +16,11 @@
     .NOTES
         AUTHOR: FULLENWARTH Luc
         LASTEDIT: 09/04/2017
-        VERSION:1.0.0
+        VERSION:1.1.0
     .LINK
         Get-OrphanForeignSecurityPrincipal.ps1
-    #>    
-    
+    #>
+
     [Alias('fofsp')]
 
     [CmdletBinding()]
@@ -28,80 +28,80 @@
     [OutputType([System.Collections.ArrayList])]
 
     Param(
-            [Parameter(HelpMessage='A file name or a full path')]
-            [String]$TabDelimitedFile
-         )
+        [Parameter(HelpMessage = 'A file name or a full path')]
+        [String]$TabDelimitedFile
+    )
 
     Begin
     {
-        Set-StrictMode -Version Latest
-
         #Requires -Module ActiveDirectory
 
-        If($PSBoundParameters['Debug'])
-        {$DebugPreference='Continue'}
+        If ($PSBoundParameters['Debug'])
+        {$DebugPreference = 'Continue'}
     }
 
     Process
     {
-        $OrphanForeignSecurityPrincipals=@()
+        $OrphanForeignSecurityPrincipals = @()
 
-        $DomainSIDList=Find-AllDomainsAndSID
+        $DomainSIDList = Find-AllDomainsAndSID
 
         Write-Verbose -Message 'Searching for orphan Foreign Security Principals...'
-        
-        $Params=@{
-                    Filter     = {ObjectClass -eq 'foreignSecurityPrincipal'}
-                    Properties = @('MemberOf','objectSid')
-                 }
-        $ForeignSecurityPrincipalList=Get-ADObject @Params
+
+        $Params = @{
+            Filter     = {ObjectClass -eq 'foreignSecurityPrincipal'}
+            Properties = @('MemberOf', 'objectSid')
+        }
+        $ForeignSecurityPrincipalList = Get-ADObject @Params
 
         Write-Verbose -Message 'Starting Foreign Security Principals name resolution...'
-        foreach($ForeignSecurityPrincipal in $ForeignSecurityPrincipalList)
+        foreach ($ForeignSecurityPrincipal in $ForeignSecurityPrincipalList)
         {
             Try #Try to resolve name
             {
-                $FSPTranslation=(New-Object System.Security.Principal.SecurityIdentifier($ForeignSecurityPrincipal.objectSid)).Translate([System.Security.Principal.NTAccount])
+                $FSPTranslation = (New-Object System.Security.Principal.SecurityIdentifier($ForeignSecurityPrincipal.objectSid)).
+                Translate([System.Security.Principal.NTAccount])
                 Write-Debug -Message "Resolved $FSPTranslation"
             }
             Catch #Find matching domain name
             {
-                $DistinguishedName=$ForeignSecurityPrincipal.DistinguishedName
-                $MemberOf=$ForeignSecurityPrincipal.MemberOf
+                $DistinguishedName = $ForeignSecurityPrincipal.DistinguishedName
+                $MemberOf = $ForeignSecurityPrincipal.MemberOf
 
                 #Search domain name
-                $DomainName='Domain not found'
-                foreach($DomainSID in $DomainSIDList.GetEnumerator())
+                $DomainName = 'Domain not found'
+                foreach ($DomainSID in $DomainSIDList.GetEnumerator())
                 {
-                    If($DistinguishedName.Contains($DomainSID.Value))
+                    If ($DistinguishedName.Contains($DomainSID.Value))
                     {
-                        $DomainName=$DomainSID.Key
+                        $DomainName = $DomainSID.Key
                         Break
                     }
                 }
-                
-                Write-Debug -Message "Could not resolve $DistinguishedName"
-                
-                $Item=[PSCustomObject]@{
-                                            DistinguishedName=$DistinguishedName
-                                            DomainName=$DomainName
-                                            MemberOf=$MemberOf -join ';'
-                                       }
 
-                $OrphanForeignSecurityPrincipals+=$Item
+                If ($DomainName -eq 'Domain not found')
+                {Write-Debug -Message "Could not resolve domain name for $DistinguishedName"}
+
+                $Item = [PSCustomObject]@{
+                    DistinguishedName = $DistinguishedName
+                    DomainName        = $DomainName
+                    MemberOf          = $MemberOf -join ';'
+                }
+
+                $OrphanForeignSecurityPrincipals += $Item
             }
         }
 
-        If($TabDelimitedFile -and $OrphanForeignSecurityPrincipals)
+        If ($TabDelimitedFile -and $OrphanForeignSecurityPrincipals)
         {
             Write-Verbose 'Exporting to Tab delimited file...'
 
-            $Params=@{
-                        NoTypeInformation = $true
-                        Path              = $TabDelimitedFile
-                        Force             = $true
-                        Delimiter         = "`t"
-                     }
+            $Params = @{
+                NoTypeInformation = $true
+                Path              = $TabDelimitedFile
+                Force             = $true
+                Delimiter         = "`t"
+            }
 
             $OrphanForeignSecurityPrincipals|Export-Csv @Params
         }
@@ -129,12 +129,10 @@ Function Find-AllDomainsAndSID
         VERSION:1.0.0
     .LINK
         Get-OrphanForeignSecurityPrincipal.ps1
-    #>    
-    
+    #>
+
     Begin
     {
-        Set-StrictMode -Version Latest
-
         #Requires -Version 4
         #Requires -Module ActiveDirectory
     }
@@ -142,34 +140,31 @@ Function Find-AllDomainsAndSID
     Process
     {
         Write-Verbose -Message 'Searching for domain SIDs and names...'
-        
-        $AllTrustedDomains=Get-ADTrust -Filter *
 
-        $DomainSIDList=@{}
+        $DomainSIDList = @{}
 
-        foreach($objDomain in $AllTrustedDomains)
+        Try {$AllTrustedDomains = Get-ADTrust -Filter *}
+        Catch
         {
-            $DomainFullName=$objDomain.Name
+            Write-Warning -Message 'Your computer cannot run the Get-ADTrust cmdlet.
+This does not impact the orphan FSP detection but you will not know from which domain the orphan FSP is coming.'
+        }
+
+        foreach ($Domain in $AllTrustedDomains)
+        {
+            $DomainFullName = $Domain.Name
 
             Write-Debug -Message "Found $DomainFullName"
 
             #Sometimes the trust don't exist anymore or the domain is unavailable
-            Try
-            {
-                $DomainSID=(Get-ADDomain -Identity $DomainFullName).DomainSID
-            }
-            Catch
-            {
-                $DomainSID='Domain SID resolution failed'
-            }
+            Try {$DomainSID = (Get-ADDomain -Identity $DomainFullName).DomainSID}
+            Catch {$DomainSID = 'Domain SID resolution failed'}
 
             Write-Debug -Message "Resolved to $DomainSID"
 
-            $DomainSIDList.$DomainFullName=$DomainSID
+            $DomainSIDList.$DomainFullName = $DomainSID
         }
     }
 
-    End{
-            $DomainSIDList
-       }
+    End {$DomainSIDList}
 }
