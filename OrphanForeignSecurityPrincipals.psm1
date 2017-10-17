eb6d960833be46c50434c311d69127f148084344
Luc FULLENWARTH <luke.forums@outlook.com>
2017-10-17 22:21:00 +0200

    Update OrphanForeignSecurityPrincipals.psm1
    Cosmetic

--- OrphanForeignSecurityPrincipals.psm1
+++ OrphanForeignSecurityPrincipals.psm1
@@ -1,17 +1,17 @@
-﻿$ModulePath=Split-Path $MyInvocation.MyCommand.Path
+$ModulePath = Split-Path $MyInvocation.MyCommand.Path
 
-$ScriptList=Get-ChildItem -Path "$ModulePath\Scripts" -Filter '*.ps1'|
-            Where-Object -Property FullName -NotMatch '\.tests\.ps1$'
+$ScriptList = Get-ChildItem -Path "$ModulePath\Scripts" -Filter '*.ps1'|
+    Where-Object -Property FullName -NotMatch '\.tests\.ps1$'
 
-foreach($Script in $ScriptList)
+foreach ($Script in $ScriptList)
 {
     Try
-        {
-            $ScriptPath=$Script.FullName
-            . $ScriptPath
-        }
+    {
+        $ScriptPath = $Script.FullName
+        . $ScriptPath
+    }
     Catch
-        {
-            Write-Warning ("{0}: {1}" -f $ScriptPath,$_.Exception.Message)
-        }
+    {
+        Write-Warning ("{0}: {1}" -f $ScriptPath, $_.Exception.Message)
+    }
 }
